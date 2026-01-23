e63806659874c4e59ae5c41856c0e60cd8564e78
Lin Muxi <e1355399@u.nus.edu>
2026-01-23 13:51:31 +0800

    Level-0: rename chatbot to Sago and greet + exit

---
+++ src/main/java/Sago.java
@@ -0,0 +1,7 @@
+public class Sago {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Sago");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
