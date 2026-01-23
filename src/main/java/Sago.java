db2365ff41ba0560fbfb3635453d72e3023aae49
Lin Muxi <e1355399@u.nus.edu>
2026-01-23 14:31:24 +0800

    Level-2: add and list tasks

--- src/main/java/Sago.java
+++ src/main/java/Sago.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Sago {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] items = new String[100];
+        int itemCount = 0;
 
         System.out.println("Hello! I'm Sago");
         System.out.println("What can I do for you?");
@@ -10,12 +12,26 @@ public class Sago {
         while (true) {
             String userInput = scanner.nextLine();
 
+            if (userInput.trim().isEmpty()) {
+                continue;
+            }
+
+            if (userInput.equals("list")) {
+                for (int i = 0; i < itemCount; i++) {
+                    System.out.println((i + 1) + ". " + items[i]);
+                }
+                continue;
+            }
+
             if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
 
-            System.out.println(userInput);
+            items[itemCount] = userInput;
+            itemCount++;
+
+            System.out.println("add: " + userInput);
         }
 
         scanner.close();
