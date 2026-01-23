bd509d56813ffbc1c29666849419c5d6d2bc172b
Lin Muxi <e1355399@u.nus.edu>
2026-01-23 14:03:16 +0800

    Level-1: echo user input until bye

--- src/main/java/Sago.java
+++ src/main/java/Sago.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Sago {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Sago");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println(userInput);
+        }
+
+        scanner.close();
     }
 }
