42f167c5a2650b9d07c9d4f1e425cdfd84a9d416
Lin Muxi <e1355399@u.nus.edu>
2026-01-23 14:57:29 +0800

    Level-3: add mark and unmark functionality using Task class

--- src/main/java/Sago.java
+++ src/main/java/Sago.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Sago {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] items = new String[100];
-        int itemCount = 0;
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
 
         System.out.println("Hello! I'm Sago");
         System.out.println("What can I do for you?");
@@ -17,19 +17,42 @@ public class Sago {
             }
 
             if (userInput.equals("list")) {
-                for (int i = 0; i < itemCount; i++) {
-                    System.out.println((i + 1) + ". " + items[i]);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ".["
+                            + tasks[i].getStatusIcon()
+                            + "] " + tasks[i].getDescription());
                 }
                 continue;
             }
+
+            if (userInput.startsWith("mark")) {
+                int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                tasks[index].markAsDone();
+
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[X] " + tasks[index].getDescription());
+
+                continue;
+            }
+
+            if (userInput.startsWith("unmark")) {
+                int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                tasks[index].unmark();
+
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[ ] " + tasks[index].getDescription());
+
+                continue;
+            }
+
 
             if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
 
-            items[itemCount] = userInput;
-            itemCount++;
+            tasks[taskCount] = new Task(userInput);
+            taskCount++;
 
             System.out.println("add: " + userInput);
         }
