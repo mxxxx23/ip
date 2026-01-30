82c3a29bc5cd0c56b5f64ff246f8c0874ae16733
Lin Muxi <e1355399@u.nus.edu>
2026-01-31 00:41:30 +0800

    Level-8: support dates using LocalDate

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,7 +1,13 @@
-public class Deadline extends Task{
-    protected String by;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String description, String by) {
+public class Deadline extends Task{
+    protected LocalDate by;
+
+    private static final DateTimeFormatter OUTPUT_FORMAT =
+            DateTimeFormatter.ofPattern("MMM dd yyyy");
+
+    public Deadline(String description, LocalDate by) {
         super(description);
         this.by = by;
     }
@@ -16,6 +22,6 @@ public class Deadline extends Task{
         return "[" + this.getTypeIcon() + "]["
                 + this.getStatusIcon() + "] "
                 + this.description
-                + " (by: " + this.by + ")";
+                + " (by: " + by.format(OUTPUT_FORMAT) + ")";
     }
 }
