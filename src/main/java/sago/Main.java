b967e2aafa2d460466f3e9040b196d6329cfb955
Lin Muxi <e1355399@u.nus.edu>
2026-02-23 20:28:43 +0800

    Level-10: Implement basic JavaFX layout (ScrollPane, input field, send button)

--- src/main/java/sago/Main.java
+++ src/main/java/sago/Main.java
@@ -1,15 +1,45 @@
 package sago;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!");
-        Scene scene = new Scene(helloWorld);
+
+        ScrollPane scrollPane = new ScrollPane();
+        VBox dialogContainer = new VBox();
+        TextField userInput = new TextField();
+        Button sendButton = new Button("Send");
+
+        scrollPane.setContent(dialogContainer);
+        scrollPane.setFitToWidth(true);
+
+        AnchorPane root = new AnchorPane();
+        root.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        // Layout positions
+        AnchorPane.setTopAnchor(scrollPane, 0.0);
+        AnchorPane.setLeftAnchor(scrollPane, 0.0);
+        AnchorPane.setRightAnchor(scrollPane, 0.0);
+        AnchorPane.setBottomAnchor(scrollPane, 50.0);
+
+        AnchorPane.setLeftAnchor(userInput, 0.0);
+        AnchorPane.setBottomAnchor(userInput, 0.0);
+        userInput.setPrefWidth(300);
+
+        AnchorPane.setRightAnchor(sendButton, 0.0);
+        AnchorPane.setBottomAnchor(sendButton, 0.0);
+
+        Scene scene = new Scene(root, 400, 600);
         stage.setScene(scene);
         stage.show();
     }
