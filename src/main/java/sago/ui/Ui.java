6129dc952aed26e6c8a66a19228e9a5c0f86f37f
Lin Muxi <e1355399@u.nus.edu>
2026-03-17 01:43:23 +0800

    Refactor command handling and remove dead UI code

--- src/main/java/sago/ui/Ui.java
+++ src/main/java/sago/ui/Ui.java
@@ -1,8 +1,6 @@
 package sago.ui;
 
 import java.util.Scanner;
-import sago.task.Task;
-import sago.task.TaskList;
 
 /**
  * Handles user interface interactions for the application.
@@ -36,59 +34,6 @@ public class Ui {
 
     public void showMessage(String message) {
         System.out.println(message);
-    }
-
-    public void showBye(){
-        System.out.println("Bye. Hope to see you again soon!");
-    }
-
-    public void showAdded(Task task, int taskCount) {
-        System.out.println("Got it. I've added this task:");
-        System.out.println("  " + task);
-        System.out.println("Now you have " + taskCount + " tasks in the list.");
-    }
-
-    public void showDeleted(Task removed, int taskCount) {
-        System.out.println("Ok. I've removed this task:");
-        System.out.println("  " + removed);
-        System.out.println("Now you have " + taskCount + " tasks in the list.");
-    }
-
-    public void showMarked(Task task) {
-        System.out.println("Nice! I've marked this task as done:");
-        System.out.println("  " + task);
-    }
-
-    public void showUnmarked(Task task) {
-        System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println("  " + task);
-    }
-
-    public void showFindResults(TaskList matches) {
-        if (matches.size() == 0) {
-            System.out.println("No matching tasks found.");
-            return;
-        }
-
-        System.out.println("Here are the matching tasks in your list:");
-        for (int i = 0; i < matches.size(); i++) {
-            System.out.println((i + 1) + "." + matches.get(i));
-        }
-    }
-
-    /**
-     * Displays the list of tasks with their corresponding indices.
-     *
-     * @param tasks Task list to display.
-     */
-    public void showList(TaskList tasks) {
-        for (int i = 0; i < tasks.size(); i++) {
-            System.out.println((i + 1) + "." + tasks.get(i));
-        }
-    }
-
-    public void showHelp(String msg) {
-        System.out.println(msg);
     }
 
 }
